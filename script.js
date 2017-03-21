dc05afe019f0893878de31de6e966fee20afe467
Barry Pettway <barrypettway@yahoo.com>
2017-03-21 16:44:19 -0400

    final changes

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ var phone = document.getElementById('phone')
 var preview = document.getElementById('preview')
 var codeBox = document.getElementById('codebox')
 function update () {
-  var content = '<h1>Hi my name is ' + firstName.value + lastName.value + ' ! ' + ' <p>'+ describe.value + '</p> ' + ' <p>If you\'re interested in a date, you can email me at ' + email.value + ' or give me a call at</p> ' + phone.value
+  var content = '<h1>Hi my name is ' + firstName.value + '' + lastName.value + ' ! ' + ' <p>'+ describe.value + '</p> ' + ' <p>If you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email.value + '" target="_blank">'+ email.value + '</a>' + ' or give me a call at</p> ' + '<a href="tel:' + phone.value + '" target="_blank">' + phone.value + '</a>'
 
   codeBox.textContent = content
   preview.innerHTML = content
 }
 firstName.addEventListener('input', update)
+lastName.addEventListener('input', update)
+describe.addEventListener('input', update)
+email.addEventListener('input', update)
+phone.addEventListener('input', update)
